af5cd1fe774f3a4ac25f97dc1768d463ecea7548
Andrioh <andrioh00@gmail.com>
2023-12-15 21:40:54 -0300

    base do frontend completa

--- LoginTest/frontend/Js/login.js
+++ LoginTest/frontend/Js/login.js
@@ -1,5 +1,8 @@
 /* Elementos do html */
 
+// Mesagem do analise
+let pLG = document.getElementById("lg-msg")
+
 // Login
 let emailLG = document.getElementById("lg-email");
 let senhaLG = document.getElementById("lg-password");
@@ -8,6 +11,7 @@ let senhaLG = document.getElementById("lg-password");
 let formLG = document.getElementById("lg-form");
 
 /* Funções */
-formLG.addEventListener("submit", function(){
+formLG.addEventListener("submit", function(event){
+    event.preventDefault();
     console.log("Submit login ativado")
 });
--- LoginTest/frontend/Js/registro.js
+++ LoginTest/frontend/Js/registro.js
@@ -1,5 +1,8 @@
 /* Elementos do html */
 
+// Mesagem do analise
+let pRG = document.getElementById("rg-msg")
+
 // Login
 let emailRG = document.getElementById("rg-email");
 let senhaRG = document.getElementById("rg-password");
@@ -8,6 +11,7 @@ let senhaRG = document.getElementById("rg-password");
 let formRG = document.getElementById("rg-form");
 
 /* Funções */
-formRG.addEventListener("submit", function(){
+formRG.addEventListener("submit", function(event){
+    event.preventDefault();
     console.log("Submit registro ativado")
 });
